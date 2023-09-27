852d8cedf6c3b2b239221298d294d9630c69baeb
Prinz Adamz <prinzadamz@gmail.com>
2023-09-27 15:28:30 +0530

    Update Register.jsx

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,13 +1,22 @@
+import { useState } from "react";
+
 export default function Register() {
+  const [inputs, setInputs] = useState({
+    fullname: "",
+    email: "",
+    username: "",
+    password: "",
+  });
+
   return (
-    <div className={'auth'}>
+    <div className={"auth"}>
       <h1>Register</h1>
       <form className={"register"}>
-        <input type="text" placeholder="Full name" />
+        <input type="text" placeholder="Full name" value />
         <input type="text" placeholder="email" />
         <input type="text" placeholder="Username" />
         <input type="password" placeholder="Password" />
-        <button type='submit'>Register</button>
+        <button type="submit">Register</button>
       </form>
     </div>
   );
