1cd219016712c3f2ef7bc224e7d28ac23560a2eb
Prinz Adamz <prinzadamz@gmail.com>
2024-03-23 14:14:26 +0530

    server setup

--- api/index.js
+++ api/index.js
@@ -1,9 +1,12 @@
 import express from 'express';
-
+import { dbConnection } from './config/dbConnection.js';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
 
+dbConnection();
 
 app.listen(7070, ()=>{
-    console.log('App is listening on port 7070 !!');
+    console.log('App is listening on port 7070 !!'); 
 })
