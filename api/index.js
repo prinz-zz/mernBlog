58c68368aac6631351adef59c6349a8e7765a25d
Prinz Adamz <prinzadamz@gmail.com>
2024-03-23 15:01:18 +0530

    routes

--- api/index.js
+++ api/index.js
@@ -2,8 +2,13 @@ import express from 'express';
 import { dbConnection } from './config/dbConnection.js';
 import dotenv from 'dotenv';
 dotenv.config();
+import userRoutes from './routes/userRoutes.js';
+
 
 const app = express();
+
+
+app.use('/api/user', userRoutes)
 
 dbConnection();
 
