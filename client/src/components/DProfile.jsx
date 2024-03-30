7d89a335f1be280eedc8b888a2ef7b4f882acafa
Prinz Adamz <prinzadamz@gmail.com>
2024-03-30 16:56:12 +0530

    Update DProfile.jsx

--- client/src/components/DProfile.jsx
+++ client/src/components/DProfile.jsx
@@ -5,41 +5,90 @@ import {
   TextInput,
   Alert,
   Spinner,
-  Avatar
+  Avatar,
 } from "flowbite-react";
-import { useSelector } from 'react-redux';
-
+import { useSelector } from "react-redux";
+import { useState, useEffect, useRef } from "react";
+import { getStorage, uploadBytesResumable, ref } from 'firebase/storage';
+import { app } from '../firebase';
 
 export default function DProfile() {
+  const [profileImg, setProfileImg] = useState(null);
+  const [imageFileUrl, setImageFileUrl] = useState(null);
+  const fileRef = useRef();
+  const [profileImgUploadProgress, setProfileImgUploadProgress] = useState(null);
 
-    const {photo, username, email} = useSelector(state => state.user.currentUser)
-    console.log(username);
+
+  const { photo, username, email } = useSelector(
+    (state) => state.user.currentUser
+  );
+  console.log(profileImg, imageFileUrl);
+
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setProfileImg(file);
+      setImageFileUrl(URL.createObjectURL(file));
+    }
+  };
+
+  useEffect(() =>{
+    if(profileImg){
+      uploadImg()
+    }
+  },[profileImg])
+
+
+  //////
+  const uploadImg = async()=>{
+   
+
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + profileImg.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, profileImg);
+
+    uploadTask.on('state_changed', 
+      (snapshot) => {
+      const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      setImageFileUploadProgress(progress.toFixed(0));
+      }
+    )  
+  }
+
 
   return (
-    <div className='max-w-lg mx-auto p-3 w-full'>
+    <div className="max-w-lg mx-auto p-3 w-full">
       <h1 className="my-7 text-center font-semibold text-3xl">Profile</h1>
 
-
-      <form className='flex flex-col gap-6'>       
-        <Avatar alt="User settings" img={photo} rounded bordered color="gray" size="xl"/>        
+      <form className="flex flex-col gap-6">
+        <input type="file" accept="image/*" onChange={handleImageChange} ref={fileRef} hidden/>
+        <Avatar
+          alt="User settings"
+          img={imageFileUrl || photo}
+          rounded
+          bordered
+          color="gray"
+          size="xl"
+          className="cursor-pointer"
+          onClick={()=> fileRef.current.click()}
+        />
         <TextInput
           id="username"
           type="text"
           placeholder="Username"
-          defaultValue={username}          
+          defaultValue={username}
         />
         <TextInput
           id="email"
           type="email"
           placeholder="Email"
-          defaultValue={email}        
+          defaultValue={email}
         />
-        <TextInput
-          id="password"
-          type="password"
-          placeholder="Password"   
-        />
-        <Button gradientDuoTone="purpleToBlue" outline>Update</Button>
+        <TextInput id="password" type="password" placeholder="Password" />
+        <Button gradientDuoTone="purpleToBlue" outline>
+          Update
+        </Button>
       </form>
       <div className="text-red-500 flex justify-between mt-2">
         <div className="cursor-pointer">Delete Account</div>
